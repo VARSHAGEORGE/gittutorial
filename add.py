d8c88b6b8d1cf8c656b936df37ea4fcf37f5c034
VARSHA GEORGE <20316@fisat>
2018-04-18 02:28:14 -0400

    Revert "Revert "create fn to add no.s""
    This reverts commit 9347a2d79af4d12fdee59f8c804e175cdc770346.

---
+++ add.py
@@ -0,0 +1,16 @@
+
+def add2(num1,num2):
+	s=num1+num2
+	return s
+def add3(num1,num2,num3):
+	s=num1+num2+num3
+	return s
+
+def add4(num1,num2,num3,num4):
+	s=num1+num2+num3+num4
+	return s
+
+print add2(3,4)
+print add3(5,7,5)
+
+print add4(5,7,5,4)
